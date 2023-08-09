332e6913317aabe6e11c0663022e3a463709286a
Murilo Damario <murilo.gcdamario@gmail.com>
2023-08-09 15:40:45 -0300

    Subindo alterações do frontend

--- menu/src/hooks/useFoodData.ts
+++ menu/src/hooks/useFoodData.ts
@@ -1,18 +1,18 @@
-import axios, { AxiosPromise } from "axios"
-import { FoodData } from "../interface/FoodData"
 import { useQuery } from "@tanstack/react-query"
+import axios, { AxiosPromise } from "axios"
+import { FoodData } from '../interface/FoodData'
 
 const API_URL = 'http://localhost:8080'
 
-const fecthData = async(): AxiosPromise<FoodData[]> => {
+const fetchData = async (): AxiosPromise<FoodData[]> => {
     const response = axios.get(API_URL + '/food')
     return response
 }
 
-export function useFoodData() {
+export function useFoodData(){
     const query = useQuery({
-        queryFn: fecthData,
-        queyKey: ['food-data'],
+        queryFn: fetchData,
+        queryKey: ['food-data'],
         retry: 2
     })
 
